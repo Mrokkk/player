a467d07255d20dfde7401cb101e935bf380def27
Mrokkk <mrokk@protonmail.com>
2017-08-25 22:12:15 +0200

    Minor changes

--- player.py
+++ player.py
@@ -9,24 +9,25 @@ class FileBrowser:
 
     class DirEntry(urwid.Button):
 
-        signals = ['enter']
+        signals = ['enter_dir']
 
-        def __init__(self, name, is_a_dir=False, callback=None):
+        def __init__(self, name, parent_path, is_a_dir=False, callback=None):
             super().__init__(name)
             self.name = name
+            self.parent_path = parent_path
             self.isdir = is_a_dir
             if is_a_dir:
                 self._w = urwid.AttrMap(urwid.SelectableIcon([u'▸ ', name, '/'], 0), 'dir', 'dir_focused')
             else:
                 self._w = urwid.AttrMap(urwid.SelectableIcon(['  ', name], 0), 'file', 'file_focused')
-            urwid.connect_signal(self, 'enter', callback)
+            urwid.connect_signal(self, 'enter_dir', callback)
 
         def _emit(self, signal):
             urwid.emit_signal(self, signal, self.label)
 
         def keypress(self, size, key):
-            if key == 'enter':
-                self._emit('enter')
+            if key == 'C':
+                self._emit('enter_dir')
             return key
 
         def __lt__(self, other):
@@ -35,7 +36,6 @@ class FileBrowser:
             return self.name.lower() < other.name.lower()
 
     palette = [
-        ('body', '', '', ''),
         ('head', 'yellow', 'black', '', '#a30', ''),
         ('foot', 'light gray', 'black'),
         ('file', 'white', '', '', '#fff', ''),
@@ -54,15 +54,20 @@ class FileBrowser:
         self.header = urwid.Text(self.dir_name)
         self.footer = urwid.AttrWrap(urwid.Text(self.footer_text), 'foot')
         self.view = urwid.Frame(
-            urwid.AttrWrap(self.listbox, 'body'),
+            self.listbox,
             header=urwid.AttrWrap(self.header, 'head'),
             footer=self.footer)
         urwid.register_signal(FileBrowser, 'back')
         urwid.connect_signal(self, 'back', lambda: self._change_dir('..'))
 
     def _read_dir(self):
-        self.dir_list = sorted([self.DirEntry(a, is_a_dir=os.path.isdir(os.path.join(self.dir_name, a)),
-            callback=self._callback) for a in os.listdir(self.dir_name)])
+        self.dir_list = sorted([
+            self.DirEntry(
+                a,
+                str(self.dir_name),
+                is_a_dir=os.path.isdir(os.path.join(self.dir_name, a)),
+                callback=self._callback
+            ) for a in os.listdir(self.dir_name)])
 
     def _change_dir(self, dirname):
         path = os.path.abspath(os.path.join(self.dir_name, dirname))
@@ -75,22 +80,25 @@ class FileBrowser:
     def _callback(self, dirname):
         self._change_dir(dirname)
 
-    def main(self):
-        self.screen = urwid.raw_display.Screen()
-        self.screen.set_terminal_properties(256)
-        self.loop = urwid.MainLoop(
-            self.view,
-            self.palette,
-            unhandled_input=self.unhandled_input,
-            event_loop=urwid.AsyncioEventLoop(loop=asyncio.get_event_loop()),
-            screen=self.screen)
-        self.loop.run()
-
-    def unhandled_input(self, key):
+    def _unhandled_input(self, key):
         if key in ('q','Q'):
             raise urwid.ExitMainLoop()
         if key == 'u':
             urwid.emit_signal(self, 'back')
+
+    def main(self):
+        self.screen = urwid.raw_display.Screen()
+        try:
+            self.screen.set_terminal_properties(256)
+        except:
+            pass
+        self.loop = urwid.MainLoop(
+            self.view,
+            self.palette,
+            unhandled_input=self._unhandled_input,
+            event_loop=urwid.AsyncioEventLoop(loop=asyncio.get_event_loop()),
+            screen=self.screen)
+        self.loop.run()
 
 def main():
     FileBrowser().main()
