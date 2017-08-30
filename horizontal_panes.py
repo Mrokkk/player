9d45bd45d6d5550d6c4748f844e5a786c45cdc37
Mrokkk <mrokk@protonmail.com>
2017-08-30 14:01:59 +0200

    Refactoring

--- horizontal_panes.py
+++ horizontal_panes.py
@@ -9,18 +9,16 @@ class HorizontalPanes(urwid.Columns):
 
     def __init__(self, widgets):
         super().__init__([], dividechars=1)
-        self._open_box(widgets[0])
-        self._open_box(widgets[1])
-
-    def _open_box(self, box):
-        if self.contents:
-            del self.contents[self.focus_position + 1:]
-        self.contents.append((box, self.options('weight', 50)))
-        self.focus_position = len(self.contents) - 1
+        self.contents.append((widgets[0], self.options('weight', 50)))
+        self.contents.append((widgets[1], self.options('weight', 50)))
+        self.set_focus(0)
 
     def keypress(self, size, key):
-        if key == 'ctrl w':
-            self.set_focus(0 if self.focus_position else 1)
-            return None
         return self.focus.keypress(size, key)
+
+    def unhandled_input(self, key):
+        return self.focus.unhandled_input(key)
+
+    def switch_focus(self):
+        self.set_focus(0 if self.focus_position else 1)
 
--- player.py
+++ player.py
@@ -9,12 +9,40 @@ from file_browser import *
 from horizontal_panes import *
 from playlist import *
 
+class Cli:
+
+    def handle_command(self, command):
+        if not command: return True
+        if command == 'q' or command == 'qa':
+            raise urwid.ExitMainLoop()
+        else:
+            raise RuntimeError('No such command: ' + command)
+
+
+class CliPanel(urwid.Edit):
+
+    def __init__(self, cli):
+        super().__init__()
+        self.cli = cli
+
+    def unhandled_input(self, key):
+        if key == 'enter':
+            try:
+                if self.cli.handle_command(self.get_edit_text().strip()):
+                    self.set_caption('')
+                    self.set_edit_text('')
+            except RuntimeError as exc:
+                self.set_edit_text('')
+                self.set_caption(exc.__str__())
+
+
 class Player:
 
     def __init__(self):
         self.playlist = Playlist()
         self.file_browser = FileBrowser()
-        self.cli_panel = urwid.Edit()
+        self.cli = Cli()
+        self.cli_panel = CliPanel(self.cli)
         self.panes = HorizontalPanes([self.file_browser, self.playlist])
         self.view = urwid.Frame(self.panes, footer=self.cli_panel)
         self.screen = self._create_screen()
@@ -29,18 +57,13 @@ class Player:
         if key == ':':
             self.view.focus_position = 'footer'
             self.cli_panel.set_caption(':')
-            return
-        if self.view.get_focus() == 'footer':
-            if key == 'enter':
-                if self.cli_panel.get_edit_text().strip() == 'q':
-                    raise urwid.ExitMainLoop()
-                self.cli_panel.set_caption('')
-                self.cli_panel.set_edit_text('')
-                self.view.focus_position = 'body'
+        elif key == 'ctrl w':
+            self.panes.switch_focus()
         else:
-            path = self.panes.focus.unhandled_input(key)
+            path = self.view.focus.unhandled_input(key)
             if path:
                 self.panes.contents[1][0].add(path)
+            self.view.focus_position = 'body'
 
     def _create_screen(self):
         screen = urwid.raw_display.Screen()
@@ -51,14 +74,5 @@ class Player:
         return screen
 
     def run(self):
-        self.panes.set_focus(0) # Set focus to file browser
         self.main_loop.run()
-
-
-def main():
-    Player().run()
-
-
-if __name__ == '__main__':
-    main()
 
