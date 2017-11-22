75ab78ff691701d705e8ef6064115088bbae0b8e
Mrokkk <mrokk@protonmail.com>
2017-11-22 22:34:22 +0100

    Update PlayerContext

--- playerlib/player_context.py
+++ playerlib/player_context.py
@@ -3,10 +3,14 @@
 class PlayerContext:
 
     def __init__(self):
+        self.command_handler = None
         self.command_panel = None
         self.config = None
+        self.draw_lock = None
+        self.file_browser = None
         self.playback_controller = None
         self.player_controller = None
         self.playlist = None
+        self.quit = None
         self.view = None
 
