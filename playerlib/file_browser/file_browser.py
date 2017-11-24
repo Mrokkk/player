90337f6dd78efee121801ed0314b63436caefceb
Mrokkk <mrokk@protonmail.com>
2017-11-24 23:44:59 +0100

    WIP: bookmarks

--- playerlib/file_browser/file_browser.py
+++ playerlib/file_browser/file_browser.py
@@ -5,7 +5,81 @@ import os
 import urwid
 
 from playerlib.helpers.scrollable import *
+from playerlib.helpers.helpers import *
 from .dir_entry import *
+
+class Header(urwid.WidgetWrap):
+    def __init__(self, name):
+        self._name = name
+        super().__init__(urwid.AttrWrap(urwid.Text(self._name), 'head'))
+
+    def set_text(self, text):
+        self._name = text
+        self._w.set_text(text)
+
+    def text(self):
+        return self._name
+
+
+class Bookmarks(urwid.WidgetWrap):
+
+    footer_text = 'Bookmarks'
+
+    class Bookmark(urwid.Button):
+        def __init__(self, index, path):
+            self.index = index
+            self._path = path
+            self._w = urwid.AttrMap(urwid.SelectableIcon([str(index), ' ', self._path], 0),
+                'file', 'file_focused')
+
+        def keypress(self, size, key):
+            return key
+
+        def path(self):
+            return self._path
+
+
+    def __init__(self):
+        self.header = Header('Bookmarks')
+        self.content = urwid.SimpleListWalker([])
+        self.content.append(self.header)
+        self._load_bookmarks()
+        self.listbox = urwid.ListBox(self.content)
+        super().__init__(urwid.Frame(self.listbox, footer=urwid.AttrWrap(urwid.Text(self.footer_text), 'foot')))
+
+    def __del__(self):
+        self._save_bookmarks()
+
+    def _save_bookmarks(self):
+        import json
+        path = os.path.join(os.path.expanduser('~'), '.config', 'player', 'bookmarks.json')
+        try:
+            with open(path, 'w') as f:
+                json.dump([b.path() for b in self.content if b.__class__ == self.Bookmark], f)
+        except: pass
+
+    def _load_bookmarks(self):
+        import json
+        path = os.path.join(os.path.expanduser('~'), '.config', 'player', 'bookmarks.json')
+        if os.path.exists(path):
+            with open(path, 'r') as f:
+                bookmarks = json.load(f)
+                for i, b in enumerate(bookmarks):
+                    self.content.append(self.Bookmark(i + 1, b))
+
+    def __getitem__(self, index):
+        if not index: return None
+        return self.content[index].path()
+
+    @property
+    def focused(self):
+        return self.listbox.focus.path()
+
+    def add(self, path):
+        index = len(self.content)
+        self.content.append(self.Bookmark(index, path))
+        self._save_bookmarks()
+
 
 class FileBrowser(urwid.WidgetWrap):
 
@@ -15,16 +89,20 @@ class FileBrowser(urwid.WidgetWrap):
         self.callback = add_callback
         self.error_handler = error_handler
         self.dir_name = os.getcwd()
+        self.bookmarks = []
+        self.header = Header(self.dir_name)
         self.dir_list = self._read_dir(self.dir_name)
-        self.content = urwid.SimpleListWalker(self.dir_list)
+        self.content = urwid.SimpleListWalker([])
+        self.content.append(self.header)
+        self.content.extend(self.dir_list)
         self.listbox = urwid.ListBox(self.content)
-        self.header = urwid.AttrWrap(urwid.Text(self.dir_name), 'head')
         self.last_position = 0
         self.logger = logging.getLogger('FileBrowser')
-        super().__init__(urwid.Frame(
-            self.listbox,
-            header=self.header,
-            footer=urwid.AttrWrap(urwid.Text(self.footer_text), 'foot')))
+        self.file_browser_view = urwid.Frame(self.listbox,
+            footer=urwid.AttrWrap(urwid.Text(self.footer_text), 'foot'))
+        self.bookmarks_view = Bookmarks()
+        self.view = urwid.WidgetPlaceholder(self.file_browser_view)
+        super().__init__(self.view)
 
     def _read_dir(self, path, level=0):
         return sorted([
@@ -41,7 +119,7 @@ class FileBrowser(urwid.WidgetWrap):
         self.dir_name = path
         self.dir_list = self._read_dir(self.dir_name)
         self.header.set_text(path)
-        self.content[:] = self.dir_list
+        self.content[1:] = self.dir_list
 
     def _show_dir(self, parent):
         parent_path = parent.path()
@@ -94,12 +172,15 @@ class FileBrowser(urwid.WidgetWrap):
     def _enter_selected_dir(self):
         self.last_position = self.listbox.focus_position
         self._change_dir(self.content.get_focus()[0].label)
-        self.listbox.focus_position = 0
+        try:
+            self.listbox.focus_position = 1
+        except: pass
 
     def _go_back(self):
         self._change_dir('..')
         if self.last_position:
             self.listbox.focus_position = self.last_position
+            self.last_position = 0
 
     def _replace_playlist(self):
         try:
@@ -114,7 +195,30 @@ class FileBrowser(urwid.WidgetWrap):
         except Exception as e:
             self.error_handler(str(e))
 
-    def unhandled_input(self, key):
+    def _switch_views(self):
+        if self.view.original_widget == self.file_browser_view:
+            self.view.original_widget = self.bookmarks_view
+        else:
+            self.view.original_widget = self.file_browser_view
+
+    def _add_bookmark(self):
+        self.bookmarks_view.add(self.dir_name)
+
+    def _handle_bookmarks(self, key):
+        try_to_scroll(self.listbox, key)
+        if key == 'b':
+            self._switch_views()
+        elif key == 'enter':
+            self._change_dir(self.bookmarks_view.focused)
+            self._switch_views()
+        else:
+            try:
+                index = int(key)
+                self._change_dir(self.bookmarks_view[index])
+                self._switch_views()
+            except: log_exception(self.logger)
+
+    def _handle_file_browser_view(self, key):
         try_to_scroll(self.listbox, key)
         if key == 'u':
             self._go_back()
@@ -128,8 +232,17 @@ class FileBrowser(urwid.WidgetWrap):
             self._reload()
         elif key == 'C':
             self._enter_selected_dir()
+        elif key == 'b':
+            self._switch_views()
+        elif key == 'B':
+            self._add_bookmark()
         elif key == 'home':
             self.listbox.focus_position = 0
         elif key == 'end':
             self.listbox.focus_position = len(self.content) - 1
 
+    def unhandled_input(self, key):
+        if self.view.original_widget == self.file_browser_view:
+            return self._handle_file_browser_view(key)
+        else:
+            return self._handle_bookmarks(key)
