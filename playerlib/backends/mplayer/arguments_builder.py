923f4b2b120c4591d0c93bbee006d774234380b3
Mrokkk <mrokk@protonmail.com>
2018-03-18 14:39:38 +0100

    Fix for playing CD Audio

--- playerlib/backends/mplayer/arguments_builder.py
+++ playerlib/backends/mplayer/arguments_builder.py
@@ -11,24 +11,27 @@ class ArgumentsBuilder:
 
     def _get_demuxer(self, current_track):
         try: return self._config.demuxer[current_track.path.split('.')[-1]]
-        except: return 'none'
+        except: return None
 
     def _get_cdrom_device(self):
         try: return self._config.cdrom_device
         except: return '/dev/sr0'
 
     def build(self, track):
-        return [
+        args = [
             self._config.path,
             '-ao', self._config.audio_output,
             '-noquiet',
             '-slave',
             '-cdrom-device', self._get_cdrom_device(),
             '-vo', 'null',
-            '-demuxer', self._get_demuxer(track),
             '-cache', str(self._get_cache()),
             '-ss', str(track.offset),
             '-volume', '100',
-            track.path
         ]
+        demuxer = self._get_demuxer(track)
+        if demuxer:
+            args.extend(['-demuxer', demuxer])
+        args.append(track.path)
+        return args
 
