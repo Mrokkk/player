a6332669fd8f92bdb42eb8fb0fd4a92c81cc9ebb
Mrokkk <mrokk@protonmail.com>
2018-04-25 20:19:23 +0200

    Update cueparser

--- playerlib/track/readers/cue_reader.py
+++ playerlib/track/readers/cue_reader.py
@@ -24,8 +24,8 @@ class CueReader(TracksReaderInterface):
         for t in cuesheet.tracks:
             new_track = Track()
             new_track.path = os.path.join(os.path.dirname(path), t.file)
-            new_track.artist = ', '.join(cuesheet.title)
-            new_track.title = ', '.join(t.title)
+            new_track.artist = cuesheet.title
+            new_track.title = t.title
             new_track.index = str(t.index)
             new_track.length = t.length
             new_track.length_string, new_track.time_format = self._format_seconds_and_get_format_string(t.length)
